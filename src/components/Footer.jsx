b05f6aa3dcca866de8d6b53a88ed5b26226c5192
Kristina <studenikinacz@gmail.com>
2026-02-22 17:09:19 +0100

    Add UIN

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,7 @@ function Footer() {
                 <p className='footer-text'>Klidné ubytování u moře na Sardínii.</p>
                 <p className='footer-location'>Calasetta, Sardínie</p>
                 <p className='footer-text'>CIN: IT111008C2000U1135</p>
+                <p>IUN: U1135</p>
                 <p className='footer-copy'>@ {new Date().getFullYear()} Casa Nina. Všechna práva vyhrazena.</p>
             </div>
         </footer>
